a04aa5a2d02b2c538dc598ab6b39b8fc013f0150
Armen Hovsepian <rmn.hovsepian@live.com>
2016-03-20 13:14:54 +0330

    add buttons

--- app/src/main/java/com/example/rmn/testproject/MyActivity.java
+++ app/src/main/java/com/example/rmn/testproject/MyActivity.java
@@ -4,14 +4,43 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    int counter;
+    Button add,sub;
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        counter=0;
+        add=(Button)findViewById(R.id.bAdd);
+        sub=(Button)findViewById(R.id.bSubtract);
+        display=(TextView)findViewById(R.id.tvDisplay);
+
+        add.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                counter++;
+                display.setText("Your total is "+counter);
+            }
+        });
+
+        sub.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                counter--;
+                display.setText("Your total is "+counter);
+            }
+        });
+
     }
 
 
